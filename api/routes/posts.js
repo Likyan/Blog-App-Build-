e3875c4d7e3223861b9ec2f6eceb2538e95ada93
Isaac <95680768+Likyan@users.noreply.github.com>
2024-09-24 14:15:30 +0100

    Update posts.js

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -45,7 +45,7 @@ router.delete("/:id", async (req, res) => {
     const post = await Post.findById(req.params.id);
     if (post.username === req.body.username) {
       try {
-        await post.delete(); 
+        await Post.findByIdAndDelete(req.params.id); 
         res.status(200).json("Post Has Been Deleted...");
       } catch(err) {
         res.status(500).json("Error");
